a39fcb69f424078dbc43d61b7ae04e845b035eb1
Saikat Mondal <111014057+Saikat4991@users.noreply.github.com>
2026-02-01 09:41:37 +0530

    Time: 11 ms (83.58%) | Memory: 22.3 MB (33.89%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,10 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # key:sortedSTR, values: correspoinding str
-        hashMap = defaultdict(list)
+        hashMap = {}
 
         for s in strs:
-            sorted_s = "".join(sorted(s))
-            hashMap[sorted_s].append(s)
+            sorted_s = tuple(sorted(s))
+            if sorted_s in hashMap:
+                hashMap[sorted_s].append(s)
+
+            else:
+                hashMap[sorted_s] = [s]
 
         return list(hashMap.values())
